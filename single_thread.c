058a3d948c66f08c3fde4097ddd4a9808b450ace
Sauruu <smraut143@gmail.com>
2020-11-28 01:12:06 +0530

    First Commit

---
+++ single_thread.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RUNNING 0
+#define DONE    1
+
+int protothread() {
+	static int i = 0;
+	int stepStatus = 0;
+
+	switch(i){
+		
+		case 0 : printf(" Thread 1 is execuitng \n");
+		stepStatus++;
+		printf("Stepstatus is--> %d", stepStatus);
+		return RUNNING;
+	
+		case 1: printf("Thread 2 is execuitng \n");
+		stepStatus++;
+		printf("%d", stepStatus);
+		return RUNNING;
+
+		case 2: printf("Thread 3 is execuitng \n");
+		stepStatus++;
+		printf("%d", stepStatus);
+		return RUNNING;
+
+		case 3: 
+		printf("Thread 4 is executing \n");
+		stepStatus++;
+		printf("%d", stepStatus);
+		return RUNNING;
+
+	}
+	// printf("protothread!\n");
+	return DONE;
+}
+
+int main(int argc, char *argv[]) {
+
+	int i = 0;
+	int ret;
+
+	do {
+		i++;
+		ret = protothread();
+
+	} while (ret != DONE);
+
+	printf("thread stopped after %d calls\n", i);
+
+	return 0;
+}
